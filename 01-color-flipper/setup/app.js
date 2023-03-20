7d0e25c335fc3c847d2ce80092034969eb763e5e
shawn0rion <shawnorion07@gmail.com>
2023-03-20 16:23:28 -0500

    display a random hex code color

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,25 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// create btn event
+//  get color from array with rdm idx 
+//  main.background = newColor
+//  span.color.innerHTML = newColor
+//get btn
+// get span
+let btn = document.getElementById('btn');
+let main = document.querySelector('main');
+let colorText = document.querySelector('.color');
+
+btn.addEventListener('click', handleColorSwitch);
+
+function handleColorSwitch() {
+    let thisColorIdx = colors.indexOf(main.style.backgroundColor);
+    let randomIdx = getRandomNumber(colors.length);
+    let newColor = colors[randomIdx];
+    main.style.backgroundColor = newColor;
+    document.querySelector('.color').innerHTML = newColor;
+}
+
+function getRandomNumber(length) {
+    return Math.floor(Math.random() * length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+let main = document.querySelector('main');
+let btn = document.querySelector('#btn');
+
+btn.addEventListener('click', getHex);
+
+function getHex(){
+    let code = "#";
+    for (let i = 0; i < 6; ++i){
+        randIdx = getRandomNumber(hex.length);
+        code += hex[randIdx];
+    }
+    main.style.backgroundColor = code;
+    document.querySelector('.color').innerHTML = code;
+}
+
+function getRandomNumber(length){
+    return Math.floor(Math.random() * length);
+}
