e6211b496a43eecea6833893e2c88e0fa598deb5
shawn0rion <shawnorion07@gmail.com>
2023-03-21 13:02:34 -0500

    toggle a class with a click

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,15 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+// --
+// .nav-center > .nav-header / .links > li / .social-media > li
+// document 800 px threshold, if less than, then links move
+// looking for nav button and links
+// what is height of links
+
+let links = document.querySelector('.links');
+let toggleBtn = document.querySelector('.nav-toggle');
+
+toggleBtn.addEventListener('click', () => {
+    links.classList.toggle('show-links');
+});
