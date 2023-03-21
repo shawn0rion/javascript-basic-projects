a727a7f4dcb74453ba069c6038b02587648f2c5c
shawn0rion <shawnorion07@gmail.com>
2023-03-21 14:40:15 -0500

    add a class to one item and remove it from all other items by traversingthe dom tree

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,24 @@
 //using selectors inside the element
 // traversing the dom
+
+// add listener to click events on all .question-btn
+// if click,
+//  remove show text from all questions 
+//  toggle show-text on parent [.question]
+// 
+const questions = document.querySelectorAll('.question');
+let questionBtns = document.querySelectorAll('.question-btn');
+questionBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let questionTitle = btn.parentElement;
+        let question = questionTitle.parentElement;
+        if (question.classList.contains('show-text')){
+            question.classList.remove('show-text');
+        }
+        else{
+            removeAllShowText();
+            question.classList.add('show-text');
+        }
+        question.classList.toggle('show-text');
+    })
+})
