ab010f5c8fc8d4f4dda5b392f29c9a21509c20ec
shawn0rion <shawnorion07@gmail.com>
2023-03-21 13:30:33 -0500

    listen for one event and add a class, listen for another event and remove the class

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,14 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+let overlay = document.querySelector('.modal-overlay');
+let modalBtn = document.querySelector('.modal-btn');
+let closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', () => {
+    overlay.classList.add('open-modal');
+})
+
+closeBtn.addEventListener('click', () => {
+    overlay.classList.remove('open-modal');
+})
